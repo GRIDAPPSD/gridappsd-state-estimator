732b41a383133e53f7f4b1f516812ad97ab9fd16
areiman <andrewrman@gmail.com>
2019-10-17 17:43:36 -0700

    added 123 and 9500 models to run scripts

--- state-estimator/service/run-se.sh
+++ state-estimator/service/run-se.sh
@@ -1,1 +1,2 @@
-bin/state-estimator.out $1 {\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"}} 2>se_debug.txt
+bin/state-estimator.out $1 {\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"}} 2>se_debug.txt # ieee123
+#bin/state-estimator.out $1 {\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"}} 2>se_debug.txt # ieee13nodecktassets
