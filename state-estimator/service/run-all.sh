fbcf08cfe7575c9f32e1a8ef85dfb4df054ce05d
garydblack <gary.black@pnnl.gov>
2019-10-22 10:20:17 -0700

    added ulimit directive so that core dump files would be generated when applicable

--- state-estimator/service/run-all.sh
+++ state-estimator/service/run-all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#SIMREQ={\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"}} # ieee13nodecktassets
-SIMREQ={\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"}}  # ieee123
+ulimit -c unlimited # make sure core files are allowed to be generated
+SIMREQ={\"power_system_config\":{\"Line_name\":\"_5B816B93-7A5F-B64C-8460-47C17D6E4B0F\"}} # ieee13nodecktassets
+#SIMREQ={\"power_system_config\":{\"Line_name\":\"_C1C3E687-6FFD-C753-582B-632A27E28507\"}}  # ieee123
 #SIMREQ={\"power_system_config\":{\"Line_name\":\"_AAE94E4A-2465-6F5E-37B1-3E72183A4E44\"}}  # test9500new
 pushd .
-cd ~/git/gridappsd-state-plotter/state-plotter; ./state-plotter.py $1 $SIMREQ &
+cd ~/git/gridappsd-state-plotter/state-plotter; ./state-plotter.py $1 $SIMREQ >/dev/null &
 popd
-bin/state-estimator.out $1 $SIMREQ > /dev/null
+#bin/state-estimator.out $1 $SIMREQ >/dev/null 2>/dev/null
+bin/state-estimator.out $1 $SIMREQ
